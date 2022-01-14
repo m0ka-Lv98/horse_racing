6c043c205debcffc67693e90a8d9e6a3e29ca1c0
m0ka_Lv98 <mygw_yukimasa9831@yahoo.co.jp>
2022-01-14 16:17:37 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+import math
+
+class PositionalEncoding(nn.Module):
+    """
+    Classic Attention-is-all-you-need positional encoding.
+    From PyTorch docs.
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+def generate_square_subsequent_mask(size: int):
+    """Generate a triangular (size, size) mask. From PyTorch docs."""
+    mask = (torch.triu(torch.ones(size, size)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+
+
+
